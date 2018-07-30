7470d287ac0941d7dc7fcec788a09aca1536bf35
Joel Cauti Rojas <joel.cauti@gmail.com>
2018-07-30 14:38:54 -0500

    correccion en la transmision para android 7

--- app/src/main/java/com/programadoresperuanos/www/ejemploandroid_may2018_camara_basico/Main2Activity.java
+++ app/src/main/java/com/programadoresperuanos/www/ejemploandroid_may2018_camara_basico/Main2Activity.java
@@ -3,16 +3,14 @@ package com.programadoresperuanos.www.ejemploandroid_may2018_camara_basico;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.net.Uri;
 import android.os.Build;
+import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -20,8 +18,13 @@ import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 
 import static android.Manifest.permission.CAMERA;
 import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
@@ -34,14 +37,20 @@ public class Main2Activity extends AppCompatActivity {
     ImageView fotografia;
     ImageButton botonCamara;
     TextView mostrarDetallesDeImagen;
+
     String nombreImagen;
     String rutaImagen;
-    Uri uriImagen;
-    File archivoImagen;
+
+    // Uri uriImagen;
+
+    File archivoImagenTemp;
     Bitmap bitmapImagen;
+    OutputStream transmisionImagen;
+    int calidadImagen = 85;
 
     public static final String APP_DIRECTORY = "EjemploCamara/";
     public static final String MEDIA_DIRECTORY = APP_DIRECTORY+"Capturas";
+    public static final int REQUEST_IMAGE_CAPTURE = 100 ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -105,20 +114,22 @@ public class Main2Activity extends AppCompatActivity {
     }
 
     private void tomarFoto() {
-        File carpeta = new File(Environment.getExternalStorageDirectory(),MEDIA_DIRECTORY);
-        boolean estado_carpeta = carpeta.exists();
-        if(!estado_carpeta){
-            estado_carpeta = carpeta.mkdirs();
+        PackageManager administrador = getApplicationContext().getPackageManager();
+        if(administrador.hasSystemFeature(PackageManager.FEATURE_CAMERA))
+        {
+            Intent camara = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            //Tambien se puede usar: Intent camara = new Intent("android.media.action.IMAGE_CAPTURE");
+            if(camara.resolveActivity(getPackageManager()) != null)
+            {
+                //Anterior, no valido para android 7 en adelante: camara.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(archivoImagen));
+                startActivityForResult(camara,REQUEST_IMAGE_CAPTURE);
+            }
         }
-        if(estado_carpeta){
-            nombreImagen = String.valueOf(System.currentTimeMillis()/1000)+".jpg";
-            rutaImagen = Environment.getExternalStorageDirectory()+File.separator+MEDIA_DIRECTORY+File.separator+nombreImagen;
-            archivoImagen = new File(rutaImagen);
-            Intent camara = new Intent("android.media.action.IMAGE_CAPTURE");
-            //Tambien se puede usar****>>>> Intent camara = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-            camara.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(archivoImagen));
-            startActivityForResult(camara,100);
+        else
+        {
+            Toast.makeText(getApplicationContext(),"Tu dispositivo no cuenta con un manejador de camaras compatible.",Toast.LENGTH_LONG).show();
         }
+
     }
 
     @Override
@@ -167,12 +178,49 @@ public class Main2Activity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
         if(resultCode == RESULT_OK){
             switch (requestCode){
-                case 100:
-                    bitmapImagen = BitmapFactory.decodeFile(rutaImagen);
+                case REQUEST_IMAGE_CAPTURE:
+                    File carpeta = new File(Environment.getExternalStorageDirectory(),MEDIA_DIRECTORY);
+                    boolean estado_carpeta = carpeta.exists();
+                    if(!estado_carpeta){
+                        estado_carpeta = carpeta.mkdirs();
+                    }
+                    if(estado_carpeta){
+                        nombreImagen = String.valueOf(System.currentTimeMillis()/1000);
+                        try {
+                            archivoImagenTemp = File.createTempFile(
+                                    nombreImagen,
+                                    ".jpg",
+                                    carpeta
+                            );
+                            //Tambien puede usar: File Imagen = new File(carpeta,Environment.getExternalStorageDirectory()+File.separator+MEDIA_DIRECTORY+File.separator+nombreImagen+".jpg");
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        rutaImagen = archivoImagenTemp.getAbsolutePath();
+                        //Tambien puede usar: rutaImagen = Environment.getExternalStorageDirectory()+File.separator+MEDIA_DIRECTORY+File.separator+nombreImagen+".jpg";
+                        transmisionImagen = null;
+                        try {
+                            transmisionImagen = new FileOutputStream(archivoImagenTemp);
+                        } catch (FileNotFoundException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                    Bundle extras = data.getExtras();
+                    bitmapImagen = (Bitmap) extras.get("data"); //BitmapFactory.decodeFile(rutaImagen);
                     fotografia.setImageBitmap(bitmapImagen);
+                    bitmapImagen.compress(Bitmap.CompressFormat.JPEG,calidadImagen,transmisionImagen);
+                    try {
+                        transmisionImagen.flush();
+                        transmisionImagen.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
                     mostrarDetallesDeImagen.setText("Ubicacion: "+rutaImagen+"\r\n"+"Nombre de la Imagen: "+nombreImagen);
                     break;
             }
         }
     }
+
+
 }
